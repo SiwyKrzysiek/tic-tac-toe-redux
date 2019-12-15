a1c702c66ade212e4baf85944a0dda46b501364e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-15 19:13:19 +0100

    Display prgress bar under active player

--- src/components/playersStatus.js
+++ src/components/playersStatus.js
@@ -1,14 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Player from "./player";
+import { connect } from "react-redux";
 
 function PlayersStatus(props) {
+  const isPlayer1Active = props.activePlayer === 0;
+  const isPlayer2Active = props.activePlayer !== 0;
+
   return (
     <div className="d-flex justify-content-center my-3">
-      <Player className="mr-auto" playerName="Player 1" isActive={true} />
-      <Player playerName="Player 2" isActive={false} />
+      <Player className="mr-auto" playerName="Player 1" isActive={isPlayer1Active} />
+      <Player playerName="Player 2" isActive={isPlayer2Active} />
     </div>
   );
 }
 
-export default PlayersStatus;
+PlayersStatus.propTypes = {
+  activePlayer: PropTypes.number.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return {
+    activePlayer: state.activePlayer
+  };
+};
+
+export default connect(
+  mapStateToProps
+)(PlayersStatus);
